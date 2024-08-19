f606d38ff0d7438a3ccddd0affc21392b06bf83d
hai.trang <trangsihai@gmail.com>
2024-08-19 14:40:35 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,131 @@
-import Image from 'next/image';
+import clsx from "clsx";
+import { Alfa_Slab_One } from "next/font/google";
+import Image from "next/image";
 
-import Hero from './hero';
-import MemePeggy from './meme-peggy';
-import About from './about';
-import Tokenomics from './tokenomics';
-import JoinUs from './join-us';
+const alfaFont = Alfa_Slab_One({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--alfa-slab-one-font",
+});
 
-import heroBg from './hero.png';
+import heroBg from "./hero.png";
 
 export default function Home() {
   return (
-    <div className='mx-auto max-w-screen-3xl'>
-      <div className='relative overflow-hidden bg-[url(./background-1.svg)] bg-cover'>
-        <Image src={heroBg} quality={100} priority alt='' />
-        <div className='absolute inset-0 overflow-hidden px-5 lg:px-10'>
-          <Hero />
+    <div className="mx-auto max-w-[1440px] bg-white">
+      <div className="relative overflow-hidden">
+        {/* banner */}
+        <div className="relative">
+          <Image src={heroBg} quality={100} priority alt="hero" />
+
+          <div className="absolute top-[5%] left-[5%] flex gap-1">
+            <a href={process.env.NEXT_PUBLIC_DEXS_URL}>
+              <img className="w-[60px] lg:w-auto" src="/new/x-icon.png" alt="" />
+            </a>
+            <a href={process.env.NEXT_PUBLIC_DEXS_URL}>
+              <img className="w-[60px] lg:w-auto" src="/new/d-icon.png" alt="" />
+            </a>
+          </div>
+          <div className="absolute top-[50%] right-[3%] -translate-y-2/4">
+            <a href={process.env.NEXT_PUBLIC_DEXS_URL}>
+              <img className="w-[120px] lg:w-auto float-right" src="/new/fefe.png" alt="" />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className='relative overflow-hidden bg-[url(./background-2.svg)] bg-cover py-10 lg:py-20'>
-        <MemePeggy />
-        <div className='mx-10 mt-10'>
-          <About />
+
+        <div>
+          <article
+            className={clsx(
+              "text-center text-[40px] text-[#C7021E] px-[10%] py-8 break-all",
+              alfaFont.variable
+            )}
+          >
+            CA: 0xfefe157c9d0aE025213092ff9a5cB56ab492BaB8
+          </article>
         </div>
-        <div className='mx-5 mt-10 lg:mx-10'>
-          <Tokenomics />
+
+        <div className="bg-[#FAE517] px-[10%] py-12">
+          <div
+            className={clsx(
+              "text-center text-[40px] text-[#C7021E] break-words",
+              alfaFont.variable
+            )}
+          >
+            FEFE, the biggest meme on TRON, every chain needs its Fefe, every
+            fefe has its day.
+          </div>
+          <div>
+            <div
+              className={clsx(
+                "text-right text-[40px] text-[#C7021E] -rotate-12 pt-[100px] pb-[50px] pr-[20px] pl-[25%] break-words",
+                alfaFont.variable
+              )}
+            >
+              Everything is just fine, FEFE is very serious frog,  brings many
+              sunshines, many fun and much love to TRON.
+            </div>
+          </div>
+
+          <div className="flex justify-evenly mt-8">
+            <img className="animate-bounce" src="/new/down-vector.png" alt="" />
+            <img className="animate-bounce" src="/new/down-vector.png" alt="" />
+            <img className="animate-bounce" src="/new/down-vector.png" alt="" />
+          </div>
         </div>
-        <div className='mx-5 -mt-10 sm:mx-10 lg:mx-20'>
-          <JoinUs />
+
+        <div>
+          <a href={process.env.NEXT_PUBLIC_DEXS_URL}>
+            <img
+              className="w-full max-w-[1440px] m-auto"
+              src="/new/buy-free.png"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="bg-[#FAE517] px-[15%] py-8">
+          <div
+            className={clsx(
+              "text-[48px] text-[#C7021E] mb-8 break-words",
+              alfaFont.variable
+            )}
+          >
+            Tokenomics token supply: 420,690,000
+          </div>
+          <div
+            className={clsx(
+              "text-[32px] text-[#C7021E] break-words",
+              alfaFont.variable
+            )}
+          >
+            No Taxes, No Bullshit. It&apos;s that simple.
+          </div>
+          <div
+            className={clsx(
+              "text-[32px] text-[#C7021E] break-words",
+              alfaFont.variable
+            )}
+          >
+            LP tokens are burnt, and contract ownership is renounced.
+          </div>
+        </div>
+
+        <div>
+          <img
+            className="w-full max-w-[1440px] m-auto"
+            src="/new/img-1.png"
+            alt=""
+          />
+        </div>
+
+        {/* footer */}
+        <div
+          className={clsx(
+            "text-center text-[40px] text-[#C7021E] px-[10%] pt-8",
+            alfaFont.variable
+          )}
+        >
+          © 2024 by Fefe. All rights reserved!
         </div>
       </div>
     </div>
